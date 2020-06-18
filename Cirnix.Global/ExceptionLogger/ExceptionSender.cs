2438ffe1ce801062448e65aa485d8f54f9fe3c07
평타천국 <pshx104@naver.com>
2020-06-19 00:43:50 +0900

    백신 차단이나 .NET이 설치되지 않은 경우 특수 예외 처리

--- Cirnix.Global/ExceptionLogger/ExceptionSender.cs
+++ Cirnix.Global/ExceptionLogger/ExceptionSender.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using System.ComponentModel;
 using System.IO;
+using CirnoLib;
+using System.Diagnostics;
 
 namespace Cirnix.Global
 {
@@ -30,8 +32,22 @@ namespace Cirnix.Global
         private static void MailAsyncSender_DoWork(object sender, DoWorkEventArgs e)
         {
             // Global Exception Catcher
-            ExceptionSendState ess = (ExceptionSendState)e.Argument;
-            File.AppendAllLines($"{Globals.ResourcePath}\\CirnixError.log", new string[] { ess.ex.Message, ess.ex.StackTrace });
+            Exception ex = ((ExceptionSendState)e.Argument).ex;
+            if (ex is FileLoadException || ex is BadImageFormatException)
+            {
+                MetroDialog.OK("백신에 의해 차단됨", "필요한 파일을 불러올 수 없었습니다.\n백신에서 허용 처리나 예외 설정을 해주세요.");
+                Globals.ProgramShutDown.Invoke();
+            }
+            else if (ex is MissingMethodException)
+            {
+                MetroDialog.OK("필요한 프로그램이 설치되지 않음", ".NET Framework 4.6.2 가 설치되어 있지 않습니다.\n확인을 누르면 치르닉스가 종료되면서 다운로드 페이지로 갑니다.");
+                Process.Start("https://www.microsoft.com/ko-kr/download/confirmation.aspx?id=53345");
+                Globals.ProgramShutDown.Invoke();
+            }
+            else
+            {
+                File.AppendAllLines($"{Globals.ResourcePath}\\CirnixError.log", new string[] { ex.GetType().Name, ex.Message, ex.StackTrace });
+            }
         }
 
         internal static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
